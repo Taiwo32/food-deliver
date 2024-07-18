f3deaddf24767cf17332eea1d3948c6caad010ea
Taiwo32 <taiwosolution02@gmail.com>
2024-07-18 19:41:07 +0100

    added

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,23 @@
 
 
 
+// import mongoose from "mongoose";
+
+// // export const DB = process.env.MONGO_URI
+
+// export const connectDB = async () =>{
+//     await mongoose.connect('mongodb+srv://taiwosolution02:taiwo1234@taiwo.d2mstxa.mongodb.net/food-del').then(()=>console.log("DB Connected"))
+// }
+
 import mongoose from "mongoose";
 
-// export const DB = process.env.MONGO_URI
+const connectDB = async () => {
 
-export const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://taiwosolution02:taiwo1234@taiwo.d2mstxa.mongodb.net/food-del').then(()=>console.log("DB Connected"))
+    mongoose.connection.on('connected',()=>{
+        console.log("Connection Established");
+    })
+
+    await mongoose.connect(`${process.env.MONGO_URI}/food-del`)
+
 }
+export default connectDB;
